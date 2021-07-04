8d4aff87dbb918a21ba2a69fbe0b6bc80ad05781
Dev33Renan <85537259+Dev33Renan@users.noreply.github.com>
2021-07-04 17:25:10 -0300

    segundo commit

--- modules/acts/ato_01.py
+++ modules/acts/ato_01.py
@@ -3,7 +3,9 @@ def act_one():
 
 
 def invadir_predio():
-    choice = input('Invadir pelo último andar ou térreo?')
+    choice = input('''Invadir pelo último andar ou térreo?
+    [1] Último Andar
+    [2] Térreo''')
     if choice == 'Último Andar':
         return pelo_ultimo_andar()
     else:
@@ -19,7 +21,9 @@ def pelo_terreo():
 
 
 def cofre_da_vile():
-    choice = input('Resgatar equipe ou roubar um guerreiro?')
+    choice = input('''Resgatar equipe ou roubar um guerreiro?
+    [1] Regatar equipe
+    [2] Roubar um Guerreiro''')
     if choice == 'Resgatar equipe':
         return resgatar_equipe()
     else:
@@ -36,7 +40,9 @@ def roubar_um_guerreiro():
 
 
 def fugir_seguranca():
-    choice = input('Pular com estátua ou se camuflar? ')
+    choice = input('''Pular com estátua ou se camuflar? 
+    [1] Pular com estátua
+    [2] Se Camuflar''')
     if choice == 'Pular junto com a estátua':
         return pular_com_estatua()
     else:
