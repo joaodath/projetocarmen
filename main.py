8514304baca0720ff5bc3d27ad8ef8a2c792b9da
João Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-07-04 15:25:56 -0300

    init

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 #O main deve rodar imports com base na necessidade do usuário durante o jogo
 
 #import idioma
+print
+language = str(input('''Escolha seu idioma:
+Choose your language: '''))
+# if: 
+#     pass
