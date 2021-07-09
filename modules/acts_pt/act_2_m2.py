482be0351d36da735f0138d8818fe734f1e05bb1
Dev33Renan <85537259+Dev33Renan@users.noreply.github.com>
2021-07-08 21:56:49 -0300

    meuultimocommit

--- modules/acts_pt/act_2_m2.py
+++ modules/acts_pt/act_2_m2.py
@@ -11,7 +11,30 @@ def start_m2(player):
 def in_the_lab(player):
     clear_screen()
     #Storytelling
-    print_slow('''Storytelling aqui''')
+    print_slow(f'''{player.nome} O que você quer Dra Bellum? Códigos de lançamentos? Armas Biológicas? Repetição de vídeos de gatos?
+        Dra Bellum - Eu quero que me traga um osso, e é por isso que vou lhe mandá-la a Hell Creek, Montana, EUA, para conseguir um osso de dinossauro. Tyrannosaurus Rex, para ser mais específica, um que foi descoberto com tecido mole intacto.
+
+        {player.name} – Não vai clonar........ (Dra Bellum corta a fala da Carmen) 
+     
+        Dra Bellum – Se alguém vai tentar essa proeza da natureza, serei eu!
+        {player.name} Ahhh eu Vou para Hell Creek.''')
+    sleep(1)
+    clear_screen() 
+    print_slow(f''' Player - Montana significa montanha em espanhol. Há um bilhão de anos, muitos dinossauros vagaram por Hell Creek.
+
+         {player.name} - Osso do quadril, osso da coxa. Onde está afinal esse T-Rex? 
+
+         {player.name} - com seu binóculo detecta onde está o osso e tira uma foto e envia para o Player
+
+        Player – E temos o osso de dinossauro
+
+        {player.name} - Levaram para o avião. Consegui descobrir o destino?
+
+        Player - Há um laboratório de pesquisa a 300km daí. Qual o seu plano?
+
+      ''')
+    sleep(1)
+    clear_screen()
     text = '''
     Você irá se esconder da ACME ou fugir com o osso?
 
@@ -39,7 +62,18 @@ def run(player):
 def vile_safebox(player):
     clear_screen()
     #Storytelling
-    print_slow('''Storytelling aqui''')
+    print_slow(f'''{player.name}-Alguma pista onde está Zakk e Ivy? 
+    Player – nada ainda estou procurando esse tipo de tijolos e janelas
+    {player.name}-Estamos ficando sem tempo.''')
+    sleep(1)
+    clear_screen() 
+    print_slow(f''' {player.name}- chama no comunicador a V.I.L.E e fala que não irá roubar mais se não tiver mais provas que os seus amigos estão bem.
+    {player.name}- Consegui convencer um dos chefes da V.I.L.E a mostrar mais uma vez os seus amigos e logo após ela tira uma foto e manda para o Player.
+    {player.name}- compare as duas imagens e veja o sol na janela. Quais são as chances de ter a mesma luminosidade em horários diferentes.
+    Player - Pode ser Luz artificial
+    {player.name}- ou eles estão no único lugar na terra em que sol brilha de dia e de noite nesta época do ano.
+    Player-Buscando esconderijo da V.I.L.E no círculo ártico… Encontrei um local. O quer fazer vermelha?
+    ''')
     text = '''
     Você irá resgatar seus amigos agora ou roubar mais uma vez?
 
